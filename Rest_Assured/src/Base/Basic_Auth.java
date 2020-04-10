70a64fb2f6aff41b0839d15a57285868970fc6ee
QAnkit17 <ankit.shrivastava@tothenew.com>
2020-04-10 17:47:33 +0530

    this is the latest code plz commit .

--- Rest_Assured/src/Base/Basic_Auth.java
+++ Rest_Assured/src/Base/Basic_Auth.java
@@ -2,12 +2,24 @@ package Base;
 
 import org.testng.annotations.Test;
 
+import io.restassured.RestAssured;
+
 public class Basic_Auth 
 {   
 	
 	@Test
 	public void Basic_Authentication_Test() 
 	{
+	  var code=RestAssured.given()
+	  .auth().preemptive()
+	  .basic("ToolsQA", "TestPassword")
+	  .when()
+	  .get("http://restapi.demoqa.com/authentication/CheckForAuthentication")
+	  .getStatusCode();
+	  
+	  
+		System.out.println("The status code is"+"  "+code);
+		
 		
 
 	}
